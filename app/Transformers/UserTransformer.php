380483d112915afa8a61d0376e11cf9c54c78c1b
Hendro Prabowo <awebened@gmail.com>
2018-07-12 14:48:10 +0700

    create users table

---
+++ app/Transformers/UserTransformer.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Transformers;
+
+use App\User;
+use League\Fractal\TransformerAbstract;
+
+class UserTransformer extends TransformerAbstract
+{
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(User $user)
+    {
+        $user_role = $user->role;
+        $role = null;
+        $premium = null;
+
+        if($user_role == 1) $role = 'Pemilik Kos';
+        elseif ($user_role == 2) $role = 'Anak Kos';
+        else $role = 'Admin';
+
+        if($user->is_premium == 0) $premium = 'No';
+        else $premium = 'Yes';
+
+        return [
+            'id'        => $user->id,
+            'name'      => $user->name,
+            'email'     => $user->email,
+            'role'      => $role,
+            'credit'    => $user->credit,
+            'Premium Account'   => $premium,
+            'registered'    => $user->created_at->diffForHumans(),
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/users/all', 'UserController@index');
+Route::get('/register', 'UserController@store');
